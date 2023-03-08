43491e9db56e0802111da992b5dc98cb0c001548
dixk3458 <dixk3458@naver.com>
2023-03-08 15:47:14 +0900

    VideoCard 스타일링

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import FakeYoutube from '../api/fakeYoutube';
+import FakeYoutube from '../api/fakeYoutubeClient';
 import Youtube from '../api/youtube';
 import { useYoutubeApi } from '../context/YoutubeApiContext';
 
@@ -21,7 +21,7 @@ export default function Videos() {
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong</p>}
       {videos && (
-        <ul>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:gird-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
           {videos.map(video => {
             return <VideoCard key={video.id} video={video} />;
           })}
